477ea270459be021716563de4474bbde03c8e80c
sevenrats <sevenrats@tutanota.com>
2024-09-27 13:14:52 -0400

    kw_only dataclasses preserve kw order, implement taglist

--- tailscale_cli/v0/model.py
+++ tailscale_cli/v0/model.py
@@ -3,8 +3,60 @@ from typing import Optional, List, Union
 import json
 from datetime import datetime
 
-@dataclass
-class NodeInfo:
+class TagList(list):
+    def __init__(self, initial_list=None):
+        if initial_list is not None:
+            if not isinstance(initial_list, list):
+                raise TypeError("Initial value must be a list")
+            # Validate and normalize the initial list
+            initial_list = [self._validate_item(item) for item in initial_list]
+        else:
+            initial_list = []
+        # Call the parent list class's __init__ with the validated list
+        super().__init__(initial_list)
+
+    def _validate_item(self, item):
+        if not isinstance(item, str):
+            raise TypeError("Only strings are allowed")
+        if not item.startswith("tag:"):
+            item = "tag:" + item
+        return item
+    
+    def __contains__(self, item):
+        # Call the original __contains__ method to check for the item as is
+        if super().__contains__(item):
+            return True
+        # Check if the item exists without "tag:" prefix
+        if isinstance(item, str):
+            if not item.startswith("tag:"):
+                return super().__contains__(f'tag:{item}')
+            return super().__contains__(item)
+        return False
+    
+    def append(self, item):
+        item = self._validate_item(item)
+        super().append(item)
+
+    def extend(self, iterable):
+        validated_items = [self._validate_item(i) for i in iterable]
+        super().extend(validated_items)
+
+    def __setitem__(self, index, item):
+        item = self._validate_item(item)
+        super().__setitem__(index, item)
+
+    def insert(self, index, item):
+        item = self._validate_item(item)
+        super().insert(index, item)
+
+    def __iadd__(self, other):
+        if not isinstance(other, list):
+            raise TypeError("Only lists can be added with '+='")
+        self.extend(other)  # Use the extend method to ensure validation
+        return self
+
+@dataclass(kw_only=True)
+class NodeInfo():
     ID: str
     PublicKey: str
     HostName: str
@@ -13,6 +65,7 @@ class NodeInfo:
     UserID: int
     TailscaleIPs: Optional[List[str]]
     AllowedIPs: Optional[List[str]]
+    Tags: Optional[TagList] = None
     Addrs: List[str]
     CurAddr: str
     Relay: str
@@ -28,10 +81,21 @@ class NodeInfo:
     Active: bool
     PeerAPIURL: Optional[List[str]]
     Capabilities: Optional[List[str]]
+    CapMap: Optional[dict] = None
     InNetworkMap: bool
     InMagicSock: bool
     InEngine: bool
-    CapMap: Optional[dict] = None
+
+    def serialize(self) -> str:
+        """Serializes the object to JSON."""
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
+    @classmethod
+    def deserialize(cls, data) -> 'Status':
+        if isinstance(data, str):
+            data = json.loads(data)
+        data['Tags'] = TagList(data['Tags']) if 'Tags' in data else TagList()
+        return cls(**data)
 
 @dataclass
 class Self(NodeInfo):
@@ -54,7 +118,7 @@ class Status:
     MagicDNSSuffix: str
     CurrentTailnet: Optional[str]
     CertDomains: Optional[List[str]]
-    Peer: Optional[str]
+    Peer: Optional[List[Peer]]
     User: Optional[str]
     ClientVersion: Optional[str]
 
@@ -62,11 +126,10 @@ class Status:
         """Serializes the object to JSON."""
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
-    @staticmethod
-    def deserialize(data: str) -> 'Status':
-        """Deserializes JSON string to a Status object."""
-        print(data)
-        dict_data = json.loads(data)
-        dict_data['Self'] = Self(**dict_data['Self'])
-        dict_data['Peer'] = [Peer(**value) for value in dict_data['Peer'].values()]
-        return Status(**dict_data)
+    @classmethod
+    def deserialize(cls, data) -> 'Status':
+        if isinstance(data, str):
+            data = json.loads(data)
+        data['Self'] = Self.deserialize(data['Self'])
+        data['Peer'] = [Peer.deserialize(value) for value in data['Peer'].values()]
+        return cls(**data)
